29bcc2c9c1a8d33be3b3ae2c26c044abee375c48
Manuel Fernandez <manferlo81@gmail.com>
2020-01-22 17:50:55 +0700

    minor refactoring

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,7 @@ function repeat(s: string, count: number): string {
   return r
 }
 
-const symbols = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-
-function f1(num: number, i: number): string {
+function f1(num: number, i: number, symbols: string[]): string {
   const unity = symbols[i]
   const five = symbols[i + 1]
   const ten = symbols[i + 2]
@@ -26,18 +24,18 @@ function f1(num: number, i: number): string {
   ]
   const r = db[num % 10]
   const nnn = Math.floor(num / 10)
-  return nnn > 0 ? `${f1(nnn, i + 2)}${r}` : r
+  return nnn > 0 ? `${f1(nnn, i + 2, symbols)}${r}` : r
 }
 
 function convert(num: number): string {
   const n = Math.floor(+num)
   if (n < 0) {
-    throw new TypeError('number can\'t be less than zero.')
+    throw new RangeError('number can\'t be less than zero.')
   }
   if (n >= 4000) {
-    throw new RangeError('number can\' be greater than 3999.')
+    throw new RangeError('number can\'t be greater than 3999.')
   }
-  return f1(n, 0)
+  return f1(n, 0, ['I', 'V', 'X', 'L', 'C', 'D', 'M'])
 }
 
 export default convert
