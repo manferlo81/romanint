780363dcc99d5a96a1e2f43b82cc723b6a6780b3
Manuel Fernandez <manferlo81@gmail.com>
2020-01-22 18:02:03 +0700

    dry and refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,24 @@
-function repeat(s: string, count: number): string {
-  let r = ''
-  while (r.length < count) {
-    r += s
-  }
-  return r
-}
-
 function f1(num: number, i: number, symbols: string[]): string {
   const unity = symbols[i]
   const five = symbols[i + 1]
   const ten = symbols[i + 2]
+  const unity2 = `${unity}${unity}`
+  const unity3 = `${unity2}${unity}`
   const db = [
     '',
     unity,
-    repeat(unity, 2),
-    repeat(unity, 3),
+    unity2,
+    unity3,
     `${unity}${five}`,
     five,
     `${five}${unity}`,
-    `${five}${repeat(unity, 2)}`,
-    `${five}${repeat(unity, 3)}`,
+    `${five}${unity2}`,
+    `${five}${unity3}`,
     `${unity}${ten}`,
   ]
-  const r = db[num % 10]
-  const nnn = Math.floor(num / 10)
-  return nnn > 0 ? `${f1(nnn, i + 2, symbols)}${r}` : r
+  const result = db[num % 10]
+  const next = Math.floor(num / 10)
+  return next > 0 ? `${f1(next, i + 2, symbols)}${result}` : result
 }
 
 function convert(num: number): string {
