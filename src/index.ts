6bb635b8a703e9423a7ec290592262c7c56d2323
Manuel Fernandez <manferlo81@gmail.com>
2020-02-20 13:26:31 +0700

    chore: optimizations

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,26 @@
 function f1(num: number, i: number, symbols: string[]): string {
   const unity = symbols[i]
+  const unity2 = unity + unity
+  const unity3 = unity2 + unity
   const five = symbols[i + 1]
-  const ten = symbols[i + 2]
-  const unity2 = `${unity}${unity}`
-  const unity3 = `${unity2}${unity}`
   const db = [
     '',
     unity,
     unity2,
     unity3,
-    `${unity}${five}`,
+    unity + five,
     five,
-    `${five}${unity}`,
-    `${five}${unity2}`,
-    `${five}${unity3}`,
-    `${unity}${ten}`,
+    five + unity,
+    five + unity2,
+    five + unity3,
+    unity + symbols[i + 2],
   ]
   const result = db[num % 10]
   const next = Math.floor(num / 10)
-  return next > 0 ? `${f1(next, i + 2, symbols)}${result}` : result
+  return next > 0 ? (f1(next, i + 2, symbols) + result) : result
 }
 
-function convert(num: number): string {
+function toRoman(num: number): string {
   const n = Math.floor(+num)
   if (n < 0) {
     throw new RangeError('number can\'t be less than zero.')
@@ -29,7 +28,7 @@ function convert(num: number): string {
   if (n >= 4000) {
     throw new RangeError('number can\'t be greater than 3999.')
   }
-  return f1(n, 0, ['I', 'V', 'X', 'L', 'C', 'D', 'M'])
+  return f1(n, 0, 'IVXLCDM'.split(''))
 }
 
-export default convert
+export default toRoman
