a487c4f90d2a151120dfe4269273f55d60e4f9c3
Manuel Fernández <manferlo81@gmail.com>
2025-04-29 15:56:00 +0700

    chore(lint): Setup `eslint-plugin-import-x`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import pluginJavascript from '@eslint/js'
 import pluginStylistic from '@stylistic/eslint-plugin'
+import { flatConfigs } from 'eslint-plugin-import-x'
 import globals from 'globals'
 import { config, configs as pluginTypescriptConfigs } from 'typescript-eslint'
 
@@ -14,7 +15,21 @@ const javascriptPluginConfig = config(
   }),
 )
 
+const importPluginConfig = config(
+  flatConfigs.recommended,
+  flatConfigs.typescript,
+  normalizeRulesConfig('import-x', {
+    'consistent-type-specifier-style': 'error',
+    'no-useless-path-segments': 'error',
+    'no-absolute-path': 'error',
+    'no-cycle': 'error',
+    'no-nodejs-modules': 'error',
+  }),
+)
+
 const stylisticPluginConfig = config(
+  // Disable rule until @stylistic/eslint-plugin types are fixed
+  // eslint-disable-next-line import-x/no-named-as-default-member
   pluginStylistic.configs.customize({
     indent: 2,
     semi: false,
@@ -50,6 +65,7 @@ export default config(
   { languageOptions: { globals: { ...globals.node, ...globals.browser } } },
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   javascriptPluginConfig,
+  importPluginConfig,
   stylisticPluginConfig,
   typescriptPluginConfig,
 )
