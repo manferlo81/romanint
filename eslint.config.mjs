4f19de114bb831f8077223b8e2b0fbf25ce029d4
Manuel Fernández <manferlo81@gmail.com>
2025-09-07 11:44:52 +0700

    chore: Cleanup eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,80 +4,103 @@ import { flatConfigs } from 'eslint-plugin-import-x'
 import globals from 'globals'
 import { config, configs as pluginTypescriptConfigs } from 'typescript-eslint'
 
-const javascriptPluginConfig = config(
+// Javascript Plugin
+
+const rulesPluginJavascript = normalizeRules(null, {
+  'no-useless-rename': 'error',
+  'object-shorthand': 'error',
+  'prefer-template': 'error',
+  'no-useless-concat': 'error',
+  eqeqeq: 'smart',
+})
+
+const configPluginJavascript = config(
   pluginJavascript.configs.recommended,
-  normalizeRulesConfig({
-    'no-useless-rename': 'error',
-    'object-shorthand': 'error',
-    'prefer-template': 'error',
-    'no-useless-concat': 'error',
-    eqeqeq: 'smart',
-  }),
+  { rules: rulesPluginJavascript },
 )
 
-const importPluginConfig = config(
+// Import Plugin
+
+const rulesPluginImport = normalizeRules('import-x', {
+  'consistent-type-specifier-style': 'error',
+  'no-useless-path-segments': 'error',
+  'no-absolute-path': 'error',
+  'no-cycle': 'error',
+  'no-nodejs-modules': 'error',
+})
+
+const configPluginImport = config(
   flatConfigs.recommended,
   flatConfigs.typescript,
-  normalizeRulesConfig('import-x', {
-    'consistent-type-specifier-style': 'error',
-    'no-useless-path-segments': 'error',
-    'no-absolute-path': 'error',
-    'no-cycle': 'error',
-    'no-nodejs-modules': 'error',
-  }),
+  { rules: rulesPluginImport },
 )
 
-const stylisticPluginConfig = config(
+// Stylistic Plugin
+
+const rulesPluginStylistic = normalizeRules('@stylistic', {
+  quotes: 'single',
+  'linebreak-style': 'unix',
+  'no-extra-parens': 'all',
+  'no-extra-semi': 'error',
+  'padded-blocks': 'off',
+})
+
+const configPluginStylistic = config(
   // Disable rule until @stylistic/eslint-plugin types are fixed
   // eslint-disable-next-line import-x/no-named-as-default-member
   pluginStylistic.configs.customize({
+    quotes: 'single',
     indent: 2,
     semi: false,
     arrowParens: true,
     quoteProps: 'as-needed',
     braceStyle: '1tbs',
+    commaDangle: 'always-multiline',
+    blockSpacing: true,
+    jsx: false,
   }),
-  normalizeRulesConfig('@stylistic', {
-    quotes: 'single',
-    'linebreak-style': 'unix',
-    'no-extra-parens': 'all',
-    'no-extra-semi': 'error',
-    'padded-blocks': 'off',
-  }),
+  { rules: rulesPluginStylistic },
 )
+
+// Typescript Plugin
+
+const rulesPluginTypescript = normalizeRules('@typescript-eslint', {
+  'array-type': { default: 'array-simple', readonly: 'array-simple' },
+  'restrict-template-expressions': 'off',
+})
 
 const typescriptPluginConfig = config(
   { languageOptions: { parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname } } },
   pluginTypescriptConfigs.strictTypeChecked,
   pluginTypescriptConfigs.stylisticTypeChecked,
-  normalizeRulesConfig('@typescript-eslint', {
-    'array-type': { default: 'array-simple', readonly: 'array-simple' },
-    'restrict-template-expressions': 'off',
-  }),
-  {
-    ...pluginTypescriptConfigs.disableTypeChecked,
-    files: ['**/*.{js,mjs,cjs}'],
-  },
+  { rules: rulesPluginTypescript },
 )
+
+const configDisableJavascriptTypeCheck = config({
+  ...pluginTypescriptConfigs.disableTypeChecked,
+  files: ['**/*.{js,mjs,cjs}'],
+})
+
+// Config
 
 export default config(
   { ignores: ['dist', 'coverage'] },
   { languageOptions: { globals: { ...globals.node, ...globals.browser } } },
   { files: ['**/*.{js,mjs,cjs,ts}'] },
-  javascriptPluginConfig,
-  importPluginConfig,
-  stylisticPluginConfig,
+  configPluginJavascript,
+  configPluginImport,
+  configPluginStylistic,
   typescriptPluginConfig,
+  configDisableJavascriptTypeCheck,
 )
 
-function normalizeRulesConfig(pluginName, rules) {
-  if (!rules && pluginName) return normalizeRulesConfig(null, pluginName)
+// Helpers
+
+function normalizeRules(pluginName, rules) {
   const entries = Object.entries(rules)
-  if (!entries.length) return {}
   const normalizeEntry = createEntryNormalizer(pluginName)
   const entriesNormalized = entries.map(normalizeEntry)
-  const rulesNormalized = Object.fromEntries(entriesNormalized)
-  return { rules: rulesNormalized }
+  return Object.fromEntries(entriesNormalized)
 }
 
 function createEntryNormalizer(pluginName) {
